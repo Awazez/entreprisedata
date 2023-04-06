066d895cc27f54bcf7dc706127b4fad7c4a6063c
cherbourg@fub.fr <cherbourg@fub.fr>
2023-04-06 22:00:59 +0200

    update

--- script.py
+++ script.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import numpy as np
 
-df = pd.read_csv(products_sold.csv')
+df = pd.read_csv('file_01.csv')
 
-df = df[df.etatAdministratifEtablissement != F]
+df = df[df.etatAdministratifEtablissement != "F"]
+df.to_csv("file_final.csv", sep='\t', encoding='utf-8')
+
+
 
 
--- script.sh
+++ script.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-mkdir /tmp/some_tmp_dir                         && \
-cd /tmp/some_tmp_dir                            && \
-curl -L https://files.data.gouv.fr/insee-sirene/StockEtablissementHistorique_utf8.zip > file.zip && \
-unzip file.zip                                  && \
+curl -L https://www.data.gouv.fr/fr/datasets/r/88fbb6b4-0320-443e-b739-b4376a012c32 > file.zip && \
+unzip  file.zip                                  && \
 rm file.zip
+split -l 1000 -d StockEtablissementHistorique_utf8.csv file_
+for i in $(find file_*); do mv $i "$i.csv"; done
+for i in $(find . -type f -name "file_*.csv" -not -name "file_00.csv");
+    do echo -e "$(head -1 file_00.csv)\n$(cat $i)" > $i;
+done
 
 
